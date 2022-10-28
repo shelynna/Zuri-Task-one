5464d6b7cf2edf04dca91fc9f7b177e92e6fff09
shelynna mckynhills <sheilamakynhills@gmail.com>
2022-10-28 14:43:39 +0000

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Links from "./components/Connect";
+import Profile from "./components/Profile";
+
+function App() {
+  return (
+    <>
+    <Links/>
+    </>
+  );
+}
+
+export default App;
